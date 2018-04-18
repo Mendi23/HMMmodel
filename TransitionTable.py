7654bcd3ed775815b2d739f00108079841f47a0f
Uriel Hai <urielha@users.noreply.github.com>
2018-04-19 02:22:17 +0300

    walla

--- TransitionTable.py
+++ TransitionTable.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 
 from collections.__init__ import Counter
 
+
 class _TransitionTable(ABC):
 
     def __init__(self):
@@ -20,17 +21,23 @@ class _TransitionTable(ABC):
 
     def getCount(self, key=None):
         if key:
-            return self._counter[key]
+            return self._getCount(key)
         return self._total
 
     def addKeyValue(self, key, value):
         assert isinstance(key, Hashable)
         self._total += value
-        self._counter[key] += value
+        self._addKeyValue(key, value)
 
     @abstractmethod
     def _addFromList(self, items, itemsLen):
         pass
+
+    def _getCount(self, key):
+        return self._counter[key]
+
+    def _addKeyValue(self, key, value):
+        self._counter[key] += value
 
     def getAllItems(self):
         return self._counter.items()
@@ -39,7 +46,8 @@ class _TransitionTable(ABC):
 class TransitionTable(_TransitionTable):
 
     def _addFromList(self, items, itemsLen):
-        self._counter += Counter(items)
+        for item in items:
+            self._counter[item] += 1
 
     def computeUnknown(self, threshold, token):
         unkCounter = Counter()
@@ -47,16 +55,31 @@ class TransitionTable(_TransitionTable):
             unkCounter[(token, pair[1])] += count
         self._counter += unkCounter
 
+
 class KTransitionTable(_TransitionTable):
 
     def __init__(self, k=3):
         super().__init__()
+        self._counters = [Counter() for _ in range(k)]
         self._k = k
 
     def _addFromList(self, items, itemsLen):
-        indexPairs = product(range(itemsLen), range(1, self._k + 1))
-        indexPairs = filter(lambda p: p[0] + p[1] <= itemsLen, indexPairs)
-        self._counter += Counter([items[i:i + j] for i, j in indexPairs])
+        for j in range(0, self._k):
+            for i in range(itemsLen - j):
+                self._counter[items[i:i + j + 1]] += 1
+            # self._counters[j] += Counter(
+            #     [items[i:i + j + 1] for i in range(itemsLen - j)])
 
+        # indexPairs = product(range(itemsLen), range(1, self._k + 1))
+        # indexPairs = filter(lambda p: p[0] + p[1] <= itemsLen, indexPairs)
+        # self._counter += Counter([items[i:i + j] for i, j in indexPairs])
+
+    # def _addKeyValue(self, key, value):
+    #     assert isinstance(key, Sized)
+    #     self._counters[len(key) - 1][key] += value
+    #
+    # def _getCount(self, key):
+    #     return self._counters[len(key) - 1][key]
+    #
     # def getAllItems(self):
     #     return chain.from_iterable(map(lambda x: x.items(), self._counters))
