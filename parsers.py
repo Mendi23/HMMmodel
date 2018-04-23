b0c857c2357a646c56992552388f8395900ca6fe
Uriel Hai <urielha@users.noreply.github.com>
2018-04-23 23:32:32 +0300

    Add output parser

--- parsers.py
+++ parsers.py
@@ -2,9 +2,9 @@ import re
 
 
 class TagsParser:
-    def __init__(self, endLineTag=(".",), wordDelim=(" ",), tagDelim='/'):
+    def __init__(self, endLineTag=(".",), wordDelim=" ", tagDelim='/'):
         self.endLine = endLineTag
-        self.wordDelim = ''.join(wordDelim)
+        self.wordDelim = wordDelim
         self.tagDelim = tagDelim
 
     def parseFile(self, filePath):
@@ -13,11 +13,27 @@ class TagsParser:
             for line in f:
                 t = re.split(f"[{self.wordDelim}]", line.strip())
                 for word in t:
-                    tagPair = tuple(word.rsplit(self.tagDelim, 1))
-                    tags.append(tagPair)
-                    if tagPair[-1] in self.endLine:
+                    processedWord = self.processWord(word)
+                    tags.append(processedWord)
+                    if self.isEndLine(processedWord):
                         yield tags
                         tags = []
+
+    def processWord(self, word):
+        return tuple(word.rsplit(self.tagDelim, 1))
+
+    def isEndLine(self, tagsPair):
+        return tagsPair[-1] in self.endLine
+
+
+class TestParser():
+    def __init__(self, wordDelim=" "):
+        self.wordDelim = wordDelim
+
+    def parseFile(self, filePath):
+        with open(filePath) as f:
+            for line in f:
+                yield from re.split(f"[{self.wordDelim}]", line.strip())
 
 
 class StorageParser:
@@ -36,3 +52,25 @@ class StorageParser:
         with open(filePath, 'w') as f:
             for tags in items:
                 f.write(f"{self.wordDelim.join(tags[:-1])}{self.valueDelim}{tags[-1]}\n")
+
+
+class OutputParser:
+    def __init__(self, filePath, wordDelim=" ", tagDelim="/"):
+        self.filePath = filePath
+        self.tagDelim = tagDelim
+        self.wordDelim = wordDelim
+
+    def __enter__(self):
+        self.fd = open(self.filePath, 'w')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.fd.close()
+
+    def append(self, word, tag, first=False):
+        if not first:
+            self.fd.write(self.wordDelim)
+        self.fd.write(f"{word}{self.tagDelim}{tag}")
+
+    def breakLine(self):
+        self.fd.write("\n")
