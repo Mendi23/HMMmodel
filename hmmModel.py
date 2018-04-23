fc4d8008cddc218e9fecfb42f86b48e6e5b568e9
Uriel Hai <urielha@users.noreply.github.com>
2018-04-23 16:01:04 +0300

    cleanup and reformat

--- hmmModel.py
+++ hmmModel.py
@@ -6,85 +6,85 @@ import numpy as np
 
 
 class HmmModel:
-	class INVALID_INTERPOLATION(ValueError):
-		pass
+    class INVALID_INTERPOLATION(ValueError):
+        pass
 
-	def __init__ (self, nOrder = 2, unkThreshold = 5):
-		self.tagsTransitions = NgramTransitions(k = nOrder + 1)
-		self.wordTags = EmissionTable()
-		self.eventsTags = EmissionTable()
-		self.unknownCounter = {}
-		self.nOrder = nOrder
-		self.unkThreshold = unkThreshold
+    def __init__(self, nOrder=2, unkThreshold=5):
+        self.tagsTransitions = NgramTransitions(k=nOrder + 1)
+        self.wordTags = EmissionTable()
+        self.eventsTags = EmissionTable()
+        self.unknownCounter = {}
+        self.nOrder = nOrder
+        self.unkThreshold = unkThreshold
 
-		self.endTag = "END"
-		self.unknownToken = "*UNK*"
-		self.eventChar = '^'
-		self.signatures = {'^Aa': re.compile("^[A-Z][a-z]"), '^ing': re.compile("ing$"),
-			'^ought': re.compile("ought$"), }
+        self.endTag = "END"
+        self.unknownToken = "*UNK*"
+        self.eventChar = '^'
+        self.signatures = {'^Aa': re.compile("^[A-Z][a-z]"), '^ing': re.compile("ing$"),
+                           '^ought': re.compile("ought$"), }
 
-	def computeFromFile (self, filePath):
-		endTags = [self.endTag] * self.nOrder
-		for tags in TagsParser().parseFile(filePath):
-			self.tagsTransitions.addFromList(list((map(lambda t: t[-1], tags))) + endTags)
-			self.wordTags.addFromIterable(self._getWordsCheckSignatures(tags))
-		self.unknownCounter = dict(self.wordTags.computeUnknown(self.unkThreshold))
+    def computeFromFile(self, filePath):
+        endTags = [self.endTag] * self.nOrder
+        for tags in TagsParser().parseFile(filePath):
+            self.tagsTransitions.addFromList(list((map(lambda t: t[-1], tags))) + endTags)
+            self.wordTags.addFromIterable(self._getWordsCheckSignatures(tags))
+        self.unknownCounter = dict(self.wordTags.computeUnknown(self.unkThreshold))
 
-	def reComputeUnknown (self, newThreshold = 5):
-		if newThreshold != self.unkThreshold:
-			self.unknownCounter = dict(self.wordTags.computeUnknown(newThreshold))
-			self.unkThreshold = newThreshold
+    def reComputeUnknown(self, newThreshold=5):
+        if newThreshold != self.unkThreshold:
+            self.unknownCounter = dict(self.wordTags.computeUnknown(newThreshold))
+            self.unkThreshold = newThreshold
 
-	def _getWordsCheckSignatures (self, tags):
-		for word, tag in tags:
-			yield (word.lower(), tag)
-			self.eventsTags.addFromIterable(map(lambda x: (x[0], tag),
-				filter(lambda r: r[1].search(word), self.signatures.items())))
+    def _getWordsCheckSignatures(self, tags):
+        for word, tag in tags:
+            yield (word.lower(), tag)
+            self.eventsTags.addFromIterable(map(lambda x: (x[0], tag),
+                                                filter(lambda r: r[1].search(word), self.signatures.items())))
 
-	def loadTransitions (self, QfilePath = None, EfilePath = None):
-		parser = StorageParser()
-		for key, value in parser.Load(QfilePath):
-			self.tagsTransitions.setValue(key, value)
-			if len(key) == 1:
-				self.tagsTransitions.updateValue((), value)
-		for key, value in parser.Load(EfilePath):
-			if key[0] == self.unknownToken:
-				self.unknownCounter[key[1]] = value
-			elif key[0].startswith(self.eventChar):
-				self.eventsTags.addFromIterable((key,), value)
-			else:
-				self.wordTags.addFromIterable((key,), value)
+    def loadTransitions(self, QfilePath=None, EfilePath=None):
+        parser = StorageParser()
+        for key, value in parser.Load(QfilePath):
+            self.tagsTransitions.setValue(key, value)
+            if len(key) == 1:
+                self.tagsTransitions.updateValue((), value)
+        for key, value in parser.Load(EfilePath):
+            if key[0] == self.unknownToken:
+                self.unknownCounter[key[1]] = value
+            elif key[0].startswith(self.eventChar):
+                self.eventsTags.addFromIterable((key,), value)
+            else:
+                self.wordTags.addFromIterable((key,), value)
 
-	def writeQ (self, filePath):
-		StorageParser().Save(filePath, self.tagsTransitions.getAllItems())
+    def writeQ(self, filePath):
+        StorageParser().Save(filePath, self.tagsTransitions.getAllItems())
 
-	def writeE (self, filePath):
-		StorageParser().Save(filePath,
-			chain(self.wordTags.getAllItems(), self.eventsTags.getAllItems(),
-				map(lambda x: (self.unknownToken,) + x, self.unknownCounter.items())))
+    def writeE(self, filePath):
+        StorageParser().Save(filePath,
+                             chain(self.wordTags.getAllItems(), self.eventsTags.getAllItems(),
+                                   map(lambda x: (self.unknownToken,) + x, self.unknownCounter.items())))
 
-	def getQ (self, params, hyperParam = None):
-		""" compute q(t_n|t_1,t_2,...t_n-1)
-		based on the folowing equation:
-		"""
+    def getQ(self, params, hyperParam=None):
+        """ compute q(t_n|t_1,t_2,...t_n-1)
+        based on the folowing equation:
+        """
 
-		if not hyperParam:
-			hyperParam = (1,)+(0,)*self.nOrder
+        if not hyperParam:
+            hyperParam = (1,) + (0,) * self.nOrder
 
-		if sum(hyperParam) != 1:
-			raise self.INVALID_INTERPOLATION()
+        if sum(hyperParam) != 1:
+            raise self.INVALID_INTERPOLATION()
 
-		getTagVal = self.tagsTransitions.getValue
-		length = min(self.nOrder + 1, len(params))
-		countValues = map(lambda i: getTagVal(params[i:]) / (getTagVal(params[i:-1]) or 1),
-			range(length))
-		return sum(np.array(hyperParam) * np.fromiter(countValues, float))
+        getTagVal = self.tagsTransitions.getValue
+        length = min(self.nOrder + 1, len(params))
+        countValues = map(lambda i: getTagVal(params[i:]) / (getTagVal(params[i:-1]) or 1),
+                          range(length))
+        return sum(np.array(hyperParam) * np.fromiter(countValues, float))
 
-	# result = 0
-	# for i in range(min(self.nOrder, len(paramList)-1)):
-	# 	result += hyperParam[i]*self.tagsTransitions.getValue(paramList[
-	# 	i:])/self.tagsTransitions.getValue(paramList[i:-1])
+    # result = 0
+    # for i in range(min(self.nOrder, len(paramList)-1)):
+    # 	result += hyperParam[i]*self.tagsTransitions.getValue(paramList[
+    # 	i:])/self.tagsTransitions.getValue(paramList[i:-1])
 
-	def getE (self, w, t):
-		""" compute e(w|t) """
-		return self.wordTags.getCount(w, t) / (self.tagsTransitions.getValue((t,) or 1))
+    def getE(self, w, t):
+        """ compute e(w|t) """
+        return self.wordTags.getCount(w, t) / (self.tagsTransitions.getValue((t,) or 1))
--- parsers.py
+++ parsers.py
@@ -2,37 +2,37 @@ import re
 
 
 class TagsParser:
-	def __init__ (self, endLineTag = (".",), wordDelim = (" ",), tagDelim = '/'):
-		self.endLine = endLineTag
-		self.wordDelim = ''.join(wordDelim)
-		self.tagDelim = tagDelim
+    def __init__(self, endLineTag=(".",), wordDelim=(" ",), tagDelim='/'):
+        self.endLine = endLineTag
+        self.wordDelim = ''.join(wordDelim)
+        self.tagDelim = tagDelim
 
-	def parseFile (self, filePath):
-		tags = []
-		with open(filePath) as f:
-			for line in f:
-				t = re.split(f"[{self.wordDelim}]", line.strip())
-				for word in t:
-					tagPair = tuple(word.rsplit(self.tagDelim, 1))
-					tags.append(tagPair)
-					if tagPair[-1] in self.endLine:
-						yield tags
-						tags = []
+    def parseFile(self, filePath):
+        tags = []
+        with open(filePath) as f:
+            for line in f:
+                t = re.split(f"[{self.wordDelim}]", line.strip())
+                for word in t:
+                    tagPair = tuple(word.rsplit(self.tagDelim, 1))
+                    tags.append(tagPair)
+                    if tagPair[-1] in self.endLine:
+                        yield tags
+                        tags = []
 
 
 class StorageParser:
-	def __init__ (self, wordDelim = " ", valueDelim = "\t"):
-		self.wordDelim = wordDelim
-		self.valueDelim = valueDelim
+    def __init__(self, wordDelim=" ", valueDelim="\t"):
+        self.wordDelim = wordDelim
+        self.valueDelim = valueDelim
 
-	def Load (self, filePath):
-		if not filePath:
-			return
-		with open(filePath) as f:
-			for line in map(lambda x: x.split(self.valueDelim), f):
-				yield tuple(line[0].split(self.wordDelim)), int(line[-1])
+    def Load(self, filePath):
+        if not filePath:
+            return
+        with open(filePath) as f:
+            for line in map(lambda x: x.split(self.valueDelim), f):
+                yield tuple(line[0].split(self.wordDelim)), int(line[-1])
 
-	def Save (self, filePath, items):
-		with open(filePath, 'w') as f:
-			for tags in items:
-				f.write(f"{self.wordDelim.join(tags[:-1])}{self.valueDelim}{tags[-1]}\n")
+    def Save(self, filePath, items):
+        with open(filePath, 'w') as f:
+            for tags in items:
+                f.write(f"{self.wordDelim.join(tags[:-1])}{self.valueDelim}{tags[-1]}\n")
