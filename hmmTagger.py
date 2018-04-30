93d7d8eca464e8c8556b42baf87b1b19d3b65861
Uriel Hai <urielha@users.noreply.github.com>
2018-04-30 16:47:08 +0300

    Yalla viterbi

--- hmmTagger.py
+++ hmmTagger.py
@@ -34,7 +34,7 @@ class GreedyTagger:
             self._queue.popleft()
 
             argmax = max(self._allTags,
-                key = lambda tag: self._calcQ(tag, tuple(self._queue)) * self._calcE(word, tag))
+                key = lambda tag: self._calcQ(tuple(self._queue) + (tag,)) * self._calcE(word, tag))
 
             self._queue.append(argmax)
             outParser.append(word, argmax)
@@ -51,54 +51,53 @@ class GreedyTagger:
         tagProb = self._model.getEventRatioTuple(tag) + (self._model.getUnknownTagRatio(tag),)
         return sum(hyperParam * tagProb)
 
-    def _calcQ (self, tag, prevTags):
-        return self._model.getQ(prevTags + (tag,), self.QHyperParam)
+    def _calcQ (self, tags):
+        return self._model.getQ(tags, self.QHyperParam)
 
     @lru_cache(maxsize = 64)
     def _calcHPunkWord (self, word):
         return scaleArray((self._model.getWordEventMask(word) +
                            (not self._model.wordExists(word),)) * self.unkSigHP)
 
-
 class ViterbiTagger(GreedyTagger):
     TagVal = namedtuple("TagVal", "t r val")
-    zeroTagVal = TagVal(0,0,0)
+    zeroTagVal = TagVal("empty", "empty", 0.0)
 
     def __init__ (self, hmmmodel: HmmModel, k = 3, endLineTag = ".",
             QHyperParam = (0.4, 0.4, 0.2), unkSigHyperParam = None):
         super().__init__(hmmmodel, k, endLineTag, QHyperParam, unkSigHyperParam)
 
-        self._allTags = [self._model.startTag, ] + self._allTags
-
+        self._allTags = self._allTags
 
     def tagLine (self, line, outParser: OutputParser):
         vTable = [
             defaultdict(lambda: defaultdict(lambda: self.zeroTagVal))
             for _ in range(len(line) + 1)]
 
+        vTable[0][self._model.startTag][self._model.startTag] = self.TagVal("start", "start", 1.0)
 
-        for i, word in enumerate([self._model.startTag, ] + line):
-            for t in self._allTags:
+        for i, word in enumerate(line, 1):
+            for t in [self._model.startTag,] + self._allTags:
                 for r in self._allTags:
                     vTable[i][t][r] = self._calcTagVal(vTable, i, t, r, word)
 
         m = vTable
 
     def _calcTagVal (self, vTable, i, t, r, word):
-        start_tag = self._model.startTag
-        if i == 0 and t == r == start_tag:
-            return self.TagVal(start_tag, start_tag, val = 1)
-        elif \
-                i == 0 \
-                or (t == r == start_tag) \
-                or (t != start_tag and
-                    (i == 1 or r == start_tag)):
-            return self.TagVal(t, r, val = 0)
+        # start_tag = self._model.startTag
+        # if i == 0 and t == r == start_tag:
+        #     return self.TagVal(start_tag, start_tag, val = 1)
+        # elif \
+        #         i == 0 \
+        #         or (t == r == start_tag) \
+        #         or (t != start_tag and
+        #             (i == 1 or r == start_tag)):
+        #     return self.TagVal(t, r, val = 0)
 
         return max(
             (self.TagVal(it, t, self._calcVTableVal(vTable, i, it, t, r, word))
-                for it in self._allTags),
+                for it in [self._model.startTag,] + self._allTags),
             key = lambda tv: tv.val)
 
     def _calcVTableVal (self, vTable, i, it, t, r, word):
-        return vTable[i - 1][it][t].val * self._calcQ(r, (it, t)) * self._calcE(word, r)
+        return vTable[i - 1][it][t].val * self._calcQ((it, t, r)) * self._calcE(word, r)
