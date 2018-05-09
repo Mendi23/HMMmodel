b2134f78e53cf8a07193652b4d80c5ec93c9500a
Uriel Hai <urielha@users.noreply.github.com>
2018-05-09 20:24:43 +0300

    measuretime measuretime like you never did

--- scripts_t/measuretime.py
+++ scripts_t/measuretime.py
@@ -1,15 +1,19 @@
 from functools import wraps
 from time import time
+from datetime import datetime
 
 
 def measure(method):
     @wraps(method)
     def wrapper(*args, **kwargs):
+        mname = method.__name__
         s = time()
+        print(f"Start  {mname} at: {datetime.now()}")
         method(*args, **kwargs)
         e = time()
         mins, secs = divmod(e - s, 60)
-        print(f"Total time of {method.__name__}: {mins:02.0f}:{secs:05.2f}s")
+        print(f"Finish {mname} at: {datetime.now()}")
+        print(f"Total time of {mname}: {mins:02.0f}:{secs:05.2f}s")
 
     return wrapper
 
