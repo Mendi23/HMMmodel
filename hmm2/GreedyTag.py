c7db5ea268648aa41d476fc59b7fffa7beaa0701
Uriel Hai <urielha@users.noreply.github.com>
2018-05-10 17:26:56 +0300

    move imports down

--- hmm2/GreedyTag.py
+++ hmm2/GreedyTag.py
@@ -3,13 +3,12 @@ import sys
 from inspect import currentframe, getfile as i_getfile
 from os.path import realpath, abspath, split as p_split, join as p_join
 
-from scripts_t.measuretime import measure
-
 root_path = realpath(abspath(p_join(p_split(i_getfile(currentframe()))[0], "..")))
 if root_path not in sys.path:
     sys.path.insert(0, root_path)
 # -------------------------
 
+from scripts_t.measuretime import measure
 from hmm2.hmm_Taggers import GreedyTagger
 from utils.parsers import TestParser, OutParser
 from utils.hmmModel import HmmModel
